37905875fdaf72c4b5c7f35564a0a77329d511ca
PabloHenrique <pablohenriquenadai@gmail.com>
2022-05-08 22:43:57 -0300

    2022 05 08 - TreinoSábadoFatec

---
+++ bee1017.py
@@ -0,0 +1,7 @@
+h = int(input())
+vel = int(input())
+
+km = vel * h
+litros = km / 12
+
+print(f"{litros:.3f}")
---
+++ bee1323.py
@@ -0,0 +1,8 @@
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    total = 0
+    for i in range (n, 0, -1):
+        total += i * i
+    print(total)
